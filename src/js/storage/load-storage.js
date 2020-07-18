ee3fe1f4da21810ef546f377700d740c386b1cca
Maja Zubko <majamzubko@gmail.com>
2020-07-18 15:13:50 +0200

    fixed bug - items loaded from storage have correct unit

--- src/js/storage/load-storage.js
+++ src/js/storage/load-storage.js
@@ -4,7 +4,15 @@ import { showCounter } from '../list/counter/show-counter';
 
 export const loadStorage = () => {
 	const storedItems = getStorage();
+	console.log(storedItems);
 	storedItems.forEach((item) => {
+		//correct unit
+		if (item.type === 'pcs') {
+			item.type = 'x';
+		} else if (item.type === 'weight') {
+			item.type = 'kg';
+		}
+		//display item
 		appendList(item);
 		showCounter();
 	});
